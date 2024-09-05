7b613a05a5737cce0ce6ad7cbc7717c4a818bca2
Can Yilmaz <canyilmaz9999@gmail.com>
2024-09-05 15:32:18 +0200

    Can/05.09.2024/US'lerin TC'leri eklendi.

---
+++ src/test/java/Tests/Can/US_08.java
@@ -0,0 +1,17 @@
+package Tests.Can;
+
+import org.testng.annotations.Test;
+
+public class US_08 {
+
+    @Test
+    public void TC01(){}
+
+
+    @Test
+    public void TC02(){}
+
+
+    @Test
+    public void TC03(){}
+}
