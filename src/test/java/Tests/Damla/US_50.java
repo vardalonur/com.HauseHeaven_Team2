7fb5d0a92a4eee2702ab0b51a3bb8cba43876d70
damlacinkaya <zeynepdamlakesgin@gmail.com>
2024-09-10 01:31:14 +0300

    * Damla/09.09.2024/US_50_classıeklendi
    * damla
    
    * damla
    
    * damla
    
    * damla/06.09.2024/admindashboardEklendi
    
    * Damla/09.09.2024/US_50_classıeklendi

---
+++ src/test/java/Tests/Damla/US_50.java
@@ -0,0 +1,59 @@
+package Tests.Damla;
+
+import Pages.AdminDashboard;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US_50 {
+
+    WebDriver driver;
+
+@Test
+    public void TC01(){
+
+        // Launch browser and Go to https://qa.hauseheaven.com/admin/login
+        Driver.getDriver().get(ConfigReader.getProperty("AdminDashUrl"));
+
+        AdminDashboard adminDashboard=new AdminDashboard();
+
+        // Enter the relevant data in the Email field.
+        adminDashboard.emailUsernameForm.sendKeys(ConfigReader.getProperty("AdminGecerliEmail"));
+
+        // Enter the relevant data in the password field.
+        adminDashboard.passwordForm.sendKeys(ConfigReader.getProperty("AdminGecerliPassword"));
+
+        // Click the Sign in button.
+        adminDashboard.signInButton.click();
+
+    }
+    @Test
+    public void TC02(){
+
+        //Launch browser and Go to https://qa.hauseheaven.com/admin/login
+        Driver.getDriver().get(ConfigReader.getProperty("AdminDashUrl"));
+
+        AdminDashboard adminDashboard=new AdminDashboard();
+
+        //Enter the relevant data in the Email field.
+        adminDashboard.emailUsernameForm.sendKeys(ConfigReader.getProperty("AdminGecerliEmail"));
+
+        //Enter the relevant data in the password field.
+        adminDashboard.passwordForm.sendKeys(ConfigReader.getProperty("AdminGecerliPassword"));
+
+        //Click the Sign in button.
+        adminDashboard.signInButton.click();
+
+        //Click “Payments” from the sidebar menu.
+        adminDashboard.payments.click();
+
+        //Click "Transactions"  from the dropdown menu.
+        adminDashboard.transactionsAltBaslik.click();
+
+        //Transactions page is displayed.
+        SoftAssert softAssert=new SoftAssert();
+        softAssert.assertTrue(adminDashboard.transactionsAltBaslik.isDisplayed());
+    }
+}
